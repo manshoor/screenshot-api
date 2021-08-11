d073edf9b5082bb1b92f210b8bdc676771c20dd3
manshoor <manshoor.ahmad@gmail.com>
2021-08-11 23:42:28 +0500

    removed the debugging data, also make some changes for the production yml

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,9 +1,7 @@
 const {APP_SECRET} = require("../config/config");
 const protect = (req, res, next) => {
-  console.log(req.query.secretKey);
-  console.log(APP_SECRET);
   if (!req.query.apikey || req.query.apikey !== APP_SECRET) {
-    return res.status(403).send('Secret key is missing or incorrect' + APP_SECRET);
+    return res.status(403).send('Secret key is missing or incorrect');
   }
   next();
 };
